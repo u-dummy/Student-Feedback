0ca3ac50c4eb26fdb7f1238c04cb13d8861c1fdd
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-10-25 21:07:46 -0700

    took initial steps in setting up the primary react component and the html page

---
+++ client/src/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCourse: props.courseId,
+      featuredReview: {},
+      reviews: [],
+      summaryStats: {},
+    };
+  }
+
+  componentDidMount() {
+    this.updateFeed();
+  }
+
+  getReviews() {
+    fetch()
+      .then(data => {
+        return data.text();
+      })
+      .then(feed => {
+        let parsedFeed = JSON.parse(feed);
+        this.setState({
+          blogs: parsedFeed
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+      <p>Hello World!</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('reviewModule'));
