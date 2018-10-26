3365e0c41ef1fc4a74344263f7b86033f89a0b99
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-10-25 18:58:08 -0700

    changed user profile pics to be images of people. added schema diagram. changed file path for the database seeding files

--- database/userDataGenerator.js
+++ database/userDataGenerator.js
@@ -3,16 +3,19 @@ const faker = require('faker');
 const userDataGenerator = (userCount) => {
   const userData = [];
   for (let i = 1; i <= userCount; i += 1) {
-    const firstName = faker.name.firstName();
+    const gender = Math.floor(Math.random() * 2);
+    const firstName = faker.name.firstName(gender);
     const lastName = faker.name.lastName();
     const courseCount = Math.ceil(Math.random() * 30);
     const reviewCount = Math.ceil(Math.random() * courseCount);
-    const picNum = Math.ceil(Math.random() * 1084);
+    const picNum = Math.ceil(Math.random() * 99);
+    const picGender = (gender === 0) ? 'men' : 'women';
     let userPic;
-    Math.random() > 0.8 ? userPic = `https://picsum.photos/200/300?image=${picNum}` : userPic = firstName[0]+lastName[0];
+    Math.random() > 0.8 ? userPic = `https://randomuser.me/api/portraits/${picGender}/${picNum}.jpg` : userPic = firstName[0]+lastName[0];
     userData.push({
       username: `${firstName} ${lastName}`,
       userPic,
+      gender,
       courseCount,
       reviewCount,
     });
