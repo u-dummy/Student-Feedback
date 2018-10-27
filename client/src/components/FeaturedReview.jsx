d349a1c229bd0d3b1a839b3da5d9d1e585f37597
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-10-26 20:00:17 -0700

    rendering all the data. need to rename the endpoint next

---
+++ client/src/components/FeaturedReview.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class FeaturedReview extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (this.props.featuredReview) {
+      return (
+        <div>
+          <div>Username: {this.props.featuredReview.user.username}</div>
+          <div>Picture: {this.props.featuredReview.user.userPic}</div>
+          <div>Courses Taken: {this.props.featuredReview.user.courseCount}</div>
+          <div>Reviews Given: {this.props.featuredReview.user.reviewCount}</div>
+          <div>Rating: {this.props.featuredReview.rating}</div>
+          <div>Review: {this.props.featuredReview.review}</div>
+          <div>Date: {this.props.featuredReview.date}</div>
+          <div>Upvotes: {this.props.featuredReview.upvotes}</div>
+          <div>Downvotes: {this.props.featuredReview.downvotes}</div>
+        </div>
+      );
+    }
+    return null;
+  }
+}
+
+export default FeaturedReview;
--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -8,17 +8,16 @@ class Review extends React.Component {
   render() {
     return (
       <div>
-        <div>Username: {this.props.featuredReview.user.username}</div>
-        <div>Picture: {this.props.featuredReview.user.userPic}</div>
-        <div>Rating: {this.props.featuredReview.rating}</div>
-        <div>Review: {this.props.featuredReview.review}</div>
-        <div>Date: {this.props.featuredReview.date}</div>
-        <div>Upvotes: {this.props.featuredReview.upvotes}</div>
-        <div>Downvotes: {this.props.featuredReview.downvotes}</div>
+        <div>Username: {this.props.reviewData.user.username}</div>
+        <div>Picture: {this.props.reviewData.user.userPic}</div>
+        <div>Rating: {this.props.reviewData.rating}</div>
+        <div>Review: {this.props.reviewData.review}</div>
+        <div>Date: {this.props.reviewData.date}</div>
+        <div>Upvotes: {this.props.reviewData.upvotes}</div>
+        <div>Downvotes: {this.props.reviewData.downvotes}</div>
       </div>
     );
   }
 }
 
 export default Review;
-
