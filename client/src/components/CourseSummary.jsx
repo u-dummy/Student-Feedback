92135a20126d824a42d3df114e5016ac16c01140
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-10-26 18:04:46 -0700

    getting a some data to render in featured review. working on getting the user data to render in the featured review

---
+++ client/src/components/CourseSummary.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class CourseSummary extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default CourseSummary;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,36 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Review from './components/Review.jsx';
+import ReviewList from './components/ReviewList.jsx';
+import CourseSummary from './components/CourseSummary.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentCourse: props.courseId,
+      courseStats: {},
       featuredReview: {},
       reviews: [],
-      summaryStats: {},
     };
   }
 
   componentDidMount() {
-    this.getReviews();
+    this.getReviews(this.state.currentCourse);
   }
 
-  getReviews() {
-    fetch('/reviews/2')
-      .then((data) => {
-        return data.text();
-      }).then((data) => {
-        console.log(data);
-      })
+  getReviews(courseId) {
+    fetch(`/reviews/${courseId}`)
+      .then(rawData => (rawData.text()))
+      .then((data) => { this.setState(JSON.parse(data)) })
+      .then((data) => { console.log(this.state) })
   }
 
   render() {
     return (
       <div>
-      <p>Hello World!</p>
+        <h2>Featured review</h2>
+        <Review featuredReview={ this.state.featuredReview }/>
+        <h2>Student feedback</h2>
+        <CourseSummary stats={ this.state.courseStats }/>
+        <h2>Reviews</h2>
+        <ReviewList reviews={ this.state.reviews }/>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('reviewModule'));
+ReactDOM.render(<App courseId={1}/>, document.getElementById('reviewModule'));
