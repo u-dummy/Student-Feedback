379a42f7766c47c72ff59b1006ee959f698ce621
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-10-29 20:43:30 -0700

    rendering bold text on search. working on cleaning up code

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,32 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Search extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      query: '',
-    };
+    this.state = { query: '' };
     this.search = React.createRef();
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
- handleInputChange() {
-   this.setState({ query: this.search.current.value });
-   this.props.filterReviews(this.search.current.value);
- }
+  handleInputChange() {
+    this.setState({ query: this.search.current.value });
+    this.props.filterReviews(this.search.current.value);
+  }
 
- render() {
-   return (
-     <div>
-       <input
-         placeholder='Search reviews'
-         ref={this.search}
-         onChange={this.handleInputChange}
-       />
-       <p>{this.state.query}</p>
-     </div>
-   )
- }
+  render() {
+    return (
+      <div>
+        <input
+          placeholder='Search reviews'
+          ref={this.search}
+          onChange={this.handleInputChange}
+        />
+        <p>{this.state.query}</p>
+      </div>
+    );
+  }
 }
 
 export default Search;
