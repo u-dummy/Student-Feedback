264b556f7beadfd91db3361b16956d5146c14192
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-10-29 20:43:30 -0700

    rendering bold text on search. working on cleaning up code

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -6,6 +6,24 @@ class Review extends React.Component {
   }
 
   render() {
+    if (typeof this.props.reviewData.review === 'object') {
+      return (
+        <div>
+          <div>Username: {this.props.reviewData.user.username}</div>
+          <div>Picture: {this.props.reviewData.user.userPic}</div>
+          <div>Rating: {this.props.reviewData.rating}</div>
+          <div>
+            Review: {this.props.reviewData.review.preQuery}
+            <b>{this.props.reviewData.review.query}</b>
+            {this.props.reviewData.review.postQuery}
+            </div>
+          <div>Date: {this.props.reviewData.date}</div>
+          <div>Upvotes: {this.props.reviewData.upvotes}</div>
+          <div>Downvotes: {this.props.reviewData.downvotes}</div>
+        </div>
+      );
+    }
+
     return (
       <div>
         <div>Username: {this.props.reviewData.user.username}</div>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,6 +5,7 @@ import FeaturedReview from './components/FeaturedReview.jsx';
 import ReviewList from './components/ReviewList.jsx';
 import CourseSummary from './components/CourseSummary.jsx';
 import Search from './components/Search.jsx';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,16 +16,35 @@ class App extends React.Component {
       reviews: [],
       filteredReviews: [],
     };
-    this.filterBasedOnSearch = this.filterBasedOnSearch.bind(this);
+    this.filterAndAddDelimitersBasedOnSearch = this.filterAndAddDelimitersBasedOnSearch.bind(this);
   }
 
   componentDidMount() {
     this.getReviews(this.state.currentCourse);
   }
 
-  filterBasedOnSearch(query) {
-    const filteredReviews = this.state.reviews.filter(review => (review.review.toUpperCase().includes(query.toUpperCase())));
-    this.setState({ filteredReviews });
+  filterAndAddDelimitersBasedOnSearch(query) {
+    const filteredReviews = this.state.reviews.filter(review =>
+      (review.review.toUpperCase().includes(query.toUpperCase())));
+
+    const filteredAndDelimitedReviews = filteredReviews.map((review) => {
+      const queryStartingIndex = review.review.toUpperCase().indexOf(query.toUpperCase());
+      const queryEndingIndex = queryStartingIndex + query.length;
+      const splitReviewObj = {
+        preQuery: review.review.slice(0, queryStartingIndex),
+        query,
+        postQuery: review.review.slice(queryEndingIndex),
+      };
+      return {
+        user: review.user,
+        rating: review.rating,
+        date: review.date,
+        upvotes: review.upvotes,
+        downvotes: review.downvotes,
+        review: splitReviewObj,
+      };
+    });
+    this.setState({ filteredReviews: filteredAndDelimitedReviews });
   }
 
   getReviews(courseId) {
@@ -32,7 +52,7 @@ class App extends React.Component {
       .then(rawData => (rawData.text()))
       .then((data) => {
         this.setState(JSON.parse(data));
-        this.setState({ filteredReviews: this.state.reviews});
+        this.setState({ filteredReviews: this.state.reviews });
       });
   }
 
@@ -44,7 +64,7 @@ class App extends React.Component {
           <FeaturedReview featuredReview={ this.state.featuredReview } />
           <h2>Student feedback</h2>
           <CourseSummary stats={ this.state.courseStats }/>
-          <Search filterReviews={ this.filterBasedOnSearch } />
+          <Search filterReviews={ this.filterAndAddDelimitersBasedOnSearch } />
           <h2>Reviews</h2>
           <ReviewList reviews={ this.state.filteredReviews }/>
         </div>
