ed280a20117dfbd03735dabd3e3efe867878ce1e
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-10-30 12:19:08 -0700

    filtering based on bar graph click. working on stacking search filter with rating filter

--- client/src/components/CourseSummary.jsx
+++ client/src/components/CourseSummary.jsx
@@ -11,11 +11,16 @@ class CourseSummary extends React.Component {
     return (
       <div>
         <div>Average: {this.props.stats.avg}</div>
-        <SummaryRow percent={ this.props.stats[5] }/>
-        <SummaryRow percent={ this.props.stats[4] }/>
-        <SummaryRow percent={ this.props.stats[3] }/>
-        <SummaryRow percent={ this.props.stats[2] }/>
-        <SummaryRow percent={ this.props.stats[1] }/>
+        <SummaryRow percent={ this.props.stats[5] }
+        ratingFilter={() => (this.props.ratingFilter(5))}/>
+        <SummaryRow percent={ this.props.stats[4] }
+        ratingFilter={() => (this.props.ratingFilter(4))}/>
+        <SummaryRow percent={ this.props.stats[3] }
+        ratingFilter={() => (this.props.ratingFilter(3))}/>
+        <SummaryRow percent={ this.props.stats[2] }
+        ratingFilter={() => (this.props.ratingFilter(2))}/>
+        <SummaryRow percent={ this.props.stats[1] }
+        ratingFilter={() => (this.props.ratingFilter(1))}/>
       </div>
     );
   }
--- client/src/components/SummaryRow.jsx
+++ client/src/components/SummaryRow.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import BarGuage from './BarGuage.jsx';
 
 const SummaryRow = props => (
-  <div>
+  <div onClick={props.ratingFilter}>
     <BarGuage percent={ props.percent * 100 }/>
     <img src='https://mbtskoudsalg.com/images250_/5-stars-transparent-png-5.png'></img>
     <button>{ Math.round(props.percent * 100) }</button>
