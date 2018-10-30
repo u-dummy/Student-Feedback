1749d0104fe483da778dbf726f967697dc0a9efd
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-10-30 11:51:45 -0700

    modularized course summary section a little and put placeholder for stars image. working on filtering based on click of bar/stars/percentages

--- client/src/components/BarGuage.jsx
+++ client/src/components/BarGuage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const BarGuage = props => (
-  <div style={{ width: `${props.percent}%`, border: '1px solid black' }}></div>
+  <div style={{ width: '75%', border: '1px solid black' }}>
+    <div style={{ width: `${props.percent}%`, border: '1px solid black' }}></div>
+  </div>
 );
 
 export default BarGuage;
---
+++ client/src/components/SummaryRow.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import BarGuage from './BarGuage.jsx';
+
+const SummaryRow = props => (
+  <div>
+    <BarGuage percent={ props.percent * 100 }/>
+    <img src='https://mbtskoudsalg.com/images250_/5-stars-transparent-png-5.png'></img>
+    <button>{ Math.round(props.percent * 100) }</button>
+  </div>
+);
+
+export default SummaryRow;
