ec22559cc1f5f7119b38b39461178380bf5e319a
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-10-30 22:18:49 -0700

    did a good chunk of styling on the FeaturedReview component. going to do a bit on the StudentFeedback component

--- client/src/components/FeaturedReview.jsx
+++ client/src/components/FeaturedReview.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import moment from 'moment';
 
 class FeaturedReview extends React.Component {
   render() {
+    let userPic;
     if (this.props.featuredReview) {
+      if (this.props.featuredReview.user.userPic.length > 2) {
+        userPic = <img src={this.props.featuredReview.user.userPic}></img>;
+      } else {
+        userPic = this.props.featuredReview.user.userPic;
+      }
+
       return (
-        <div>
-          <div>Username: {this.props.featuredReview.user.username}</div>
-          <div>Picture: {this.props.featuredReview.user.userPic}</div>
-          <div>Courses Taken: {this.props.featuredReview.user.courseCount}</div>
-          <div>Reviews Given: {this.props.featuredReview.user.reviewCount}</div>
-          <div>Rating: {this.props.featuredReview.rating}</div>
-          <div>Review: {this.props.featuredReview.review}</div>
-          <div>Date: {this.props.featuredReview.date}</div>
-          <div>Upvotes: {this.props.featuredReview.upvotes}</div>
-          <div>Downvotes: {this.props.featuredReview.downvotes}</div>
+        <div className='featuredReviewContainer'>
+          <div className='featuredReviewTopRow'>
+            <div className='featureReviewUserPic'>{userPic}</div>
+            <div className='featuredReviewInfo'>
+              <span className='featuredReviewUsername'>{this.props.featuredReview.user.username}</span>
+              <span className='featuredReviewCounts'>( {this.props.featuredReview.user.courseCount} courses, {this.props.featuredReview.user.reviewCount} reviews )</span>
+              <div>Rating: {this.props.featuredReview.rating}</div>
+              <div>{moment(this.props.featuredReview.date).fromNow()}</div>
+            </div>
+          </div>
+          <div className='reviewText'>{this.props.featuredReview.review}</div>
         </div>
       );
     }
