dc9df8238908ee7cbbc0f9107461ff1f1c070211
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-10-31 21:58:56 -0700

    non-selected bars are fading when one is selected

--- client/src/components/CourseSummary.jsx
+++ client/src/components/CourseSummary.jsx
@@ -4,6 +4,12 @@ import SummaryRow from './SummaryRow.jsx';
 
 class CourseSummary extends React.Component {
   render() {
+    let fade = 1;
+
+    if (this.props.selectedStar !== null) {
+      fade = 0.25;
+    }
+
     return (
       <div>
         <h2>Student feedback</h2>
@@ -11,17 +17,17 @@ class CourseSummary extends React.Component {
           <div className='studentFeedbackAverageContainer'>
             <div className='studentFeedbackAverage'>{Number(this.props.stats.avg).toFixed(1)}</div>
           </div>
-          <div className='studentFeedbackBarGraph'>
-            <SummaryRow percent={ this.props.stats[5] } star={5}
-            ratingFilter={() => (this.props.ratingFilter(5))}/>
-            <SummaryRow percent={ this.props.stats[4] } star={4}
-            ratingFilter={() => (this.props.ratingFilter(4))}/>
-            <SummaryRow percent={ this.props.stats[3] } star={3}
-            ratingFilter={() => (this.props.ratingFilter(3))}/>
-            <SummaryRow percent={ this.props.stats[2] } star={2}
-            ratingFilter={() => (this.props.ratingFilter(2))}/>
-            <SummaryRow percent={ this.props.stats[1] } star={1}
-            ratingFilter={() => (this.props.ratingFilter(1))}/>
+          <div className='studentFeedbackBarGraph' >
+            <SummaryRow percent={ this.props.stats[5] } selectedStar={ this.props.selectedStar }
+            star={5} ratingFilter={() => (this.props.ratingFilter(5))}/>
+            <SummaryRow percent={ this.props.stats[4] } selectedStar={ this.props.selectedStar }
+            star={4} ratingFilter={() => (this.props.ratingFilter(4))}/>
+            <SummaryRow percent={ this.props.stats[3] } selectedStar={ this.props.selectedStar }
+            star={3} ratingFilter={() => (this.props.ratingFilter(3))}/>
+            <SummaryRow percent={ this.props.stats[2] } selectedStar={ this.props.selectedStar }
+            star={2} ratingFilter={() => (this.props.ratingFilter(2))}/>
+            <SummaryRow percent={ this.props.stats[1] } selectedStar={ this.props.selectedStar }
+            star={1} ratingFilter={() => (this.props.ratingFilter(1))}/>
           </div>
         </div>
       </div>
@@ -30,3 +36,5 @@ class CourseSummary extends React.Component {
 }
 
 export default CourseSummary;
+
+// num is null or your star# then do nothing, else set opacity to 25%
