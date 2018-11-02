86f04b55a6d4b92cb1d02f6ee68e9dcea11278f4
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-11-01 22:15:51 -0700

    clear search button is working. now need to conditionally render when a term has been searched

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import symbols from '../symbols.jsx';
+
 const Stars = props => (
   <div className='starsContainer'>
     <div className='emptyStarsWrapper'>
@@ -10,5 +12,26 @@ const Stars = props => (
       </div>
   </div>
 );
+// class Stars extends React.Component {
+//   createFiveGraphRows() {
+
+//   }
+
+//   render() {
+//     return (
+//       <div className='starsContainer'>
+//         <span className='star'>{symbols.star}</span>
+//         <svg>
+//           <linearGradient id="starFill">
+//             <stop offset="0%" stop-color="royalblue"/>
+//             <stop offset="40%" stop-color="royalblue"/>
+//             <stop offset="40%" stop-color="royalblue"/>
+//             <stop offset="100%" stop-color="royalblue"/>
+//           </linearGradient>
+//         </svg>
+//       </div>
+//     );
+//   }
+// }
 
 export default Stars;
