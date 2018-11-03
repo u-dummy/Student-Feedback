9a0b67152bee341ad20ba0f477898d6b799159e9
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-11-03 14:07:56 -0700

    adding PropTypes and cleaning up formatting

--- client/src/components/FeaturedReview.jsx
+++ client/src/components/FeaturedReview.jsx
@@ -1,38 +1,49 @@
 import React from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
+import styles from '../styles/FeaturedReview.css';
 
 import set from '../helperFunctions.jsx';
 
-const FeaturedReview = (props) => {
-  if (props.featuredReview) {
+class FeaturedReview extends React.Component {
+  setUserPic() {
+    const { userPic } = this.props.featuredReview.user;
+    if (userPic.includes('https')) {
+      return <img className={styles.reviewUserPic} src={userPic}></img>;
+    }
+    return <div className={styles.reviewUserInitials}>{userPic}</div>;
+  }
+
+  render() {
+    if (this.props.featuredReview) {
+      return (
+        <div>
+          <h2>Featured review</h2>
+          <div className={styles.featuredReviewContainer}>
+            <div className={styles.featuredReviewTopRow}>
+              <div className={styles.featureReviewUserPic}>{this.setUserPic()}</div>
+              <div className={styles.featuredReviewInfo}>
+                <span className={styles.featuredReviewUsername}>{this.props.featuredReview.user.username}</span>
+                <span>( {this.props.featuredReview.user.courseCount} courses, {this.props.featuredReview.user.reviewCount} reviews )</span>
+                <div>
+                  <img className={styles.featuredReviewStars} src={`https://s3.us-east-2.amazonaws.com/udemy-demo-tarik/${this.props.featuredReview.rating}+stars+white.png`}></img>
+                </div>
+                <div>{moment(this.props.featuredReview.date).fromNow()}</div>
+              </div>
+            </div>
+            <div>{this.props.featuredReview.review}</div>
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div>
         <h2>Featured review</h2>
-        <div className='featuredReviewContainer'>
-          <div className='featuredReviewTopRow'>
-            <div className='featureReviewUserPic'>{set.userPic(props.featuredReview.user.userPic)}</div>
-            <div className='featuredReviewInfo'>
-              <span className='featuredReviewUsername'>{props.featuredReview.user.username}</span>
-              <span className='featuredReviewCounts'>( {props.featuredReview.user.courseCount} courses, {props.featuredReview.user.reviewCount} reviews )</span>
-              <div>
-                <img className='reviewStars' src={`https://s3.us-east-2.amazonaws.com/udemy-demo-tarik/${props.featuredReview.rating}+stars+white.png`}></img>
-              </div>
-              <div>{moment(props.featuredReview.date).fromNow()}</div>
-            </div>
-          </div>
-          <div className='reviewText'>{props.featuredReview.review}</div>
-        </div>
+        <p><i>There are no featured reviews for this course yet...</i></p>
       </div>
     );
   }
-
-  return (
-    <div>
-      <h2>Featured review</h2>
-      <p><i>There are no featured reviews for this course yet...</i></p>
-    </div>
-  );
 };
 
 FeaturedReview.propTypes = {
