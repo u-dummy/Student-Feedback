0feddbbaf659c78ec6abf828a1d527c714542097
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-11-07 20:30:40 -0800

    working on partial stars. getting offsfsets to work but not yet dynamically

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
 
-ReactDOM.render(<App courseId={Math.floor(Math.random() * 100)}/>, document.getElementById('reviewModule'));
+ReactDOM.render(<App />, document.getElementById('reviewModule'));
