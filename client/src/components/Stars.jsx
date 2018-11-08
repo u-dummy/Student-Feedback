47296e08bb6a2a8e302289ec608a4158af23e5d5
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-11-07 22:42:20 -0800

    add keys to Star component

--- client/src/components/Stars.jsx
+++ client/src/components/Stars.jsx
@@ -6,31 +6,32 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Stars.css';
 
 class Stars extends React.Component {
-  createFiveStars() {
+  createStars() {
     const stars = [];
     let totalFill = Math.ceil(this.props.rating * 100);
     let partialFill = 0;
+    const starCount = 5;
 
-    for (let i = 0; i < 5; i += 1) {
+    for (let i = 0; i < starCount; i += 1) {
       let fill = 0;
       let star = <div></div>;
       totalFill >= 100 ? fill = 100 : fill = totalFill;
       if (fill === 100) {
         star = (
-          <span className={ styles.fullStar } style={this.props.starStyle}>
+          <span key={i} className={ styles.fullStar } style={this.props.starStyle}>
             <FontAwesomeIcon icon={ faStar } />
           </span>
         );
       } else if (fill > 0) {
         partialFill = fill;
         star = (
-          <span className={ styles.partialStar } style={this.props.starStyle} >
+          <span key={i} className={ styles.partialStar } style={this.props.starStyle} >
             <FontAwesomeIcon icon={ faStar } />
           </span>
         );
       } else {
         star = (
-          <span className={ styles.emptyStar } style={this.props.starStyle} >
+          <span key={i} className={ styles.emptyStar } style={this.props.starStyle} >
             <FontAwesomeIcon icon={ faStar } />
           </span>
         );
@@ -40,7 +41,7 @@ class Stars extends React.Component {
     }
 
     stars.push(
-      <svg width='0' height='0'>
+      <svg key={starCount + 1} width='0' height='0'>
         <linearGradient id='starEmpty'>
           <stop stopColor='#dedfe0' offset='100%' />
         </linearGradient>
@@ -61,7 +62,7 @@ class Stars extends React.Component {
   render() {
     return (
       <div>
-        {this.createFiveStars()}
+        {this.createStars()}
       </div>
     );
   }
@@ -70,7 +71,7 @@ class Stars extends React.Component {
 Stars.propTypes = {
   rating: PropTypes.number.isRequired,
   starStyle: PropTypes.shape({
-    'font-size': PropTypes.string.isRequired,
+    fontSize: PropTypes.string.isRequired,
     margin: PropTypes.string.isRequired,
   }),
 };
