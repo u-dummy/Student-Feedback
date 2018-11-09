d2ba17f1073ad10eeba203a659fdcec2086cebf8
Tarik Bellamine <tarik@Tariks-MacBook.local>
2018-11-09 14:18:22 -0800

    working on port 3002 non-containerized

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const getReviewData = require('./serverModel.js');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3002;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
